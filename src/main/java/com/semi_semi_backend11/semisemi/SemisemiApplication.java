8aee5e78e9abbf3501e3fab3c0d1feb1dfe4262a
Kho23 <wndrn2002@naver.com>
2025-09-16 16:35:40 +0900

    my first commit

---
+++ src/main/java/com/semi_semi_backend11/semisemi/SemisemiApplication.java
@@ -0,0 +1,13 @@
+package com.semi_semi_backend11.semisemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemisemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemisemiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semi_semi_backend11/semisemi/SemisemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semi_semi_backend11.semisemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemisemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
