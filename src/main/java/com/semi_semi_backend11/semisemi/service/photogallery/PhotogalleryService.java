74255fbb7f304f6c3f43a76f737a8e879662a18a
JoheoungWoo <106474486+JoheoungWoo@users.noreply.github.com>
2025-09-28 08:11:46 +0900

    이미지와 데이터를 같이 올리기 위함
    - PhotoGalleryController의 기존 코드를 PhotoGalleryService쪽으로 이동
    - Enum을 통해 , 결과 여부를 판단하도록 설정

--- src/main/java/com/semi_semi_backend11/semisemi/service/photogallery/PhotogalleryService.java
+++ src/main/java/com/semi_semi_backend11/semisemi/service/photogallery/PhotogalleryService.java
@@ -2,6 +2,8 @@ package com.semi_semi_backend11.semisemi.service.photogallery;
 
 import com.semi_semi_backend11.semisemi.dto.LoanRecordDto;
 import com.semi_semi_backend11.semisemi.dto.PhotoGalleryDto;
+import com.semi_semi_backend11.semisemi.util.GalleryResultEnum;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +11,5 @@ import java.util.Optional;
 public interface PhotogalleryService {
     List<PhotoGalleryDto> findAllGallery();
     Optional<PhotoGalleryDto> findGallery(Integer galleryId);
+    GalleryResultEnum uploadGallery(MultipartFile file, PhotoGalleryDto gallery);
 }
--- src/main/java/com/semi_semi_backend11/semisemi/service/photogallery/PhotogalleryServiceImpl.java
+++ src/main/java/com/semi_semi_backend11/semisemi/service/photogallery/PhotogalleryServiceImpl.java
@@ -4,9 +4,14 @@ import com.semi_semi_backend11.semisemi.dto.LoanRecordDto;
 import com.semi_semi_backend11.semisemi.dto.PhotoGalleryDto;
 import com.semi_semi_backend11.semisemi.entity.PhotoGallery;
 import com.semi_semi_backend11.semisemi.repository.PhotoGalleryRepository;
+import com.semi_semi_backend11.semisemi.util.GalleryResultEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,4 +50,49 @@ public class PhotogalleryServiceImpl implements PhotogalleryService {
     public Optional<PhotoGalleryDto> findGallery(Integer galleryId) {
         return repository.findById(galleryId).map(this::toDto);
     }
+
+    @Override
+    public GalleryResultEnum uploadGallery(MultipartFile file, PhotoGalleryDto gallery) {
+        // 1. 파일이 비어있는지 확인
+        String uploadDir = "images/gallery/";
+        if (file.isEmpty()) {
+            return GalleryResultEnum.FileIsEmpty;
+        }
+
+        // 1_2. 파일의 크기 확인
+        if(file.getSize() > 5 * 1024 * 1024) {
+            return GalleryResultEnum.FileTooLarge;
+        }
+
+        try {
+            // 2. 고유한 파일명 세팅을 위한 세팅
+            String filename = file.getOriginalFilename(); // 파일명
+            String originalFilename = filename;  // 원본 파일명 보존
+
+            // 파일명 중복 확인 및 고유한 파일명 생성
+            int counter = 1;
+            while (Files.exists(Paths.get(uploadDir + filename))) {
+                // 파일명이 이미 존재하면 (1), (2)와 같은 형식으로 수정
+                filename = originalFilename.replaceFirst("([^.]+)(\\.[^.]+)?$", "$1(" + counter + ")$2");
+                counter++;  // 숫자 증가
+            }
+
+            // 3. 파일 저장
+            Path filePath = Paths.get(uploadDir + filename);
+            Files.createDirectories(filePath.getParent()); // 폴더가 없으면 생성
+            Files.write(filePath, file.getBytes());
+
+            // 4. 성공 시 DB에 저장할 url 생성
+            String imgUrl = uploadDir + "/" + filename;
+            gallery.setPhotoUrl(imgUrl);
+            repository.save(toEntity(gallery));
+            return GalleryResultEnum.Success;
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            return GalleryResultEnum.Failure;
+        }
+    }
+
+
 }
