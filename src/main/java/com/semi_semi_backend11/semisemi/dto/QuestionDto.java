b81a828ed54fd18ffe1be8d17376578477c75d52
Kho23 <wndrn2002@naver.com>
2025-09-27 16:39:26 +0900

    QNA 리스트 프론트 전송 기능 추가 멤버 ID 로 조회하는 로직 구현(프론트 전송은 아직 만들지 않음) Question 에 멤버 Id 와 answer 묶이게 만듦

--- src/main/java/com/semi_semi_backend11/semisemi/dto/QuestionDto.java
+++ src/main/java/com/semi_semi_backend11/semisemi/dto/QuestionDto.java
@@ -1,19 +1,21 @@
 package com.semi_semi_backend11.semisemi.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.semi_semi_backend11.semisemi.entity.Answer;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class QuestionDto {
     private Integer questionId;
     private Integer memberId;
     private String title;
     private String content;
     private LocalDateTime createdAt;
+    private String answer;
+    private String memberName;
 }
--- src/main/java/com/semi_semi_backend11/semisemi/service/questions/QuestionsServiceImpl.java
+++ src/main/java/com/semi_semi_backend11/semisemi/service/questions/QuestionsServiceImpl.java
@@ -1,16 +1,42 @@
 package com.semi_semi_backend11.semisemi.service.questions;
 
 import com.semi_semi_backend11.semisemi.dto.QuestionDto;
+import com.semi_semi_backend11.semisemi.entity.Question;
 import com.semi_semi_backend11.semisemi.repository.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class QuestionsServiceImpl implements QuestionsService{
+    @Autowired
     private QuestionRepository repository;
+    public QuestionDto toDto(Question q){
+        String answer = null;
+
+        // 💡 2. Answer 엔티티 객체가 존재하는지 (null이 아닌지) 확인합니다.
+        if (q.getAnswer() != null)
+            // 답변이 있는 경우에만 내용을 추출합니다.
+            answer = q.getAnswer().getContent();
+        else answer = "아직 답변이 작성되지 않았습니다.";
+        return QuestionDto.builder()
+                .questionId(q.getQuestionId())
+                .title(q.getTitle())
+                .content(q.getContent())
+                .createdAt(q.getCreatedAt())
+                .memberId(q.getMemberId())
+                .answer(answer)
+                .memberName(q.getMember().getName())
+                .build();
+
+    }
     @Override
     public List<QuestionDto> questions() {
-//        return repository.findAll().stream().toList();
-        return null;
+        List<QuestionDto> list = new ArrayList<>();
+        repository.findAllWithAnswer().stream().forEach(i->
+                list.add(toDto(i))
+                );
+        return list;
     }
 }
